0f77838b62cb80c5fd49eea4f0c660bb9c9517a6
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2020-12-19 04:55:21 +0200

    Set auth routes & views

--- tags_app/tags_app/settings.py
+++ tags_app/tags_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'tags_app.urls'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
