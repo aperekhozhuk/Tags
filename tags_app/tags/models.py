627395ba93db3b897fe1c09627016d1add3d459d
Anton Perekhozhuk <pentatonixx1997@gmail.com>
2020-12-20 09:11:52 +0200

    Add django-taggit

--- tags_app/tags/models.py
+++ tags_app/tags/models.py
@@ -2,12 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from taggit.managers import TaggableManager
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     is_company_admin = models.BooleanField(default = False)
     is_customer_admin = models.BooleanField(default = False)
+    tags = TaggableManager()
 
 
 @receiver(post_save, sender = User)
